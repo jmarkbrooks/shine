c876b428de8b1ecba2f87c1f1701433c0e911053
Jonathan Mark Brooks <j.mark.brooks@gmail.com>
2017-10-15 15:50:47 -0400

    Moving to an Angular template and JSON data source.

--- app/javascript/packs/customers.js
+++ app/javascript/packs/customers.js
@@ -16,10 +16,12 @@ var CustomerSearchComponent = Component({
     <label for="keywords" class="sr-only">Keywords</label> \
       <input type="text" id="keywords" name="keywords" \
              placeholder="First Name, Last Name, or Email Address" \
-             class="form-control input-lg"> \
+             class="form-control input-lg" \
+             bindon-ngModel="keywords"> \
       <span class="input-group-btn"> \
       <input type="submit" value="Find Customers" \
-             class="btn btn-primary btn-lg"> \
+             class="btn btn-primary btn-lg" \
+             on-click="search()"> \
       </span> \
     </div> \
     </form> \
@@ -29,22 +31,32 @@ var CustomerSearchComponent = Component({
     <h1 class="h3">Results</h1> \
   </header> \
   <ol class="list-group"> \
-      <li class="list-group-item clearfix"> \
+      <li *ngFor="let customer of customers" \
+          class="list-group-item clearfix"> \
         <h3 class="pull-right"> \
           <small class="text-uppercase">Joined</small> \
-          2016-01-01 \
+          {{customer.created_at}} \
         </h3> \
         <h2 class="h3"> \
-          Pat Smith \
-          <small>psmith34</small> \
+          {{customer.first_name}} {{customer.last_name}} \
+          <small>{{customer.username}}</small> \
         </h2> \
-        <h4>pat.smith@jmarkbrooks.me</h4> \
+        <h4>{{customer.email}}</h4> \
       </li> \
   </ol> \
 </section> \
   '
 }).Class({
   constructor: function() {
+    this.customers = null;
+    this.keywords = "";
+  },
+  search: function() {
+    if (this.keywords == "pat") {
+      this.customers = RESULTS;
+    } else {
+      this.customers = [];
+    }
   }
 });
 var CustomerAppModule = NgModule({
@@ -54,4 +66,41 @@ var CustomerAppModule = NgModule({
 }).Class({
   constructor: function() {}
 });
+var RESULTS = [
+  {
+    first_name: "Pat",
+    last_name: "Smith",
+    username: "psmith",
+    email: "pat.smith@somewhere.net",
+    created_at: "2016-02-05",
+  },
+  {
+    first_name: "Patrick",
+    last_name: "Jones",
+    username: "pjpj",
+    email: "jones.p@business.net",
+    created_at: "2014-03-05",
+  },
+  {
+    first_name: "Patricia",
+    last_name: "Benjamin",
+    username: "pattyb",
+    email: "benjie@aol.info",
+    created_at: "2016-01-02",
+  },
+  {
+    first_name: "Patty",
+    last_name: "Patrickson",
+    username: "ppat",
+    email: "pppp@freemail.computer",
+    created_at: "2016-02-05",
+  },
+  {
+    first_name: "Jane",
+    last_name: "Patrick",
+    username: "janesays",
+    email: "janep@company.net",
+    created_at: "2013-01-05",
+  },
+];
 platformBrowserDynamic().bootstrapModule(CustomerAppModule);
